002c7131cbd91c12591f7a2d90bd5bfb86a7cea8
Electronica y Ciencia <electronicayciencia@gmail.com>
2021-05-09 23:50:13 +0200

    Lic server update

--- gowin_eda_license_server/lic_server.py
+++ gowin_eda_license_server/lic_server.py
@@ -3,65 +3,56 @@
 # Basic license server for educative purpose only.
 # You MUST NOT use the software without a proper license.
 
-import socket
+import socketserver
 
-HOST = ''
-PORT = 10559
+class RequestHandler(socketserver.BaseRequestHandler):
 
-def getint(conn):
-    '''Read 4 bytes from socket and return them as 32 bit big-endian integer'''
-    data = conn.recv(4)
-    i = int.from_bytes(data, byteorder='big')
-    return i
+    def handle(self):
+        print("Request from", self.client_address[0], "with data:")
 
-def marshall(datadict):
-    '''Convert a dict into a serialized binary stream'''
+        length = int.from_bytes(self.request.recv(4), byteorder='big')
+        number_of_fields = int.from_bytes(self.request.recv(4), byteorder='big')
 
-    buff = bytearray()
+        # Fields
+        for _ in range(number_of_fields):
+            size = int.from_bytes(self.request.recv(4), byteorder='big')
+            key = self.request.recv(size).decode('UTF-16BE')
+            size = int.from_bytes(self.request.recv(4), byteorder='big')
+            value = self.request.recv(size).decode('UTF-16BE')
+            
+            print("  {}: {}".format(key, value))
 
-    buff = len(datadict).to_bytes(4, byteorder='big')
-
-    for key, value in datadict.items():
-        key_bytes = key.encode('UTF-16BE')
-        value_bytes = value.encode('UTF-16BE')
-        buff = buff + (
-            len(key_bytes).to_bytes(4, byteorder='big') + 
-            key_bytes +
-            len(value_bytes).to_bytes(4, byteorder='big') + 
-            value_bytes
-        )
-
-    buff = len(buff).to_bytes(4, byteorder='big') + buff
-    return buff
+        # answer: ok, lic_expired, no_such_lic, no_more_lic
+        response_data = {'res':'ok'}
+        self.answer(response_data)
 
 
-def start():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
-        
-        while True:
-            conn, addr = s.accept()
-            with conn:
-                print('Request from', addr[0])
+    def answer(self, data):
+        '''Convert a dict into a serialized binary stream'''
+        print('Response:\n  ', data)
 
-                length = getint(conn)
-                number_of_fields = getint(conn)
+        buff = bytearray()
+        buff = len(data).to_bytes(4, byteorder='big')
 
-                # Fields
-                for _ in range(number_of_fields):
-                    size = getint(conn)
-                    key = conn.recv(size).decode('UTF-16BE')
-                    size = getint(conn)
-                    value = conn.recv(size).decode('UTF-16BE')
-                    print("  {}: {}".format(key, value))
+        for key, value in data.items():
+            key_bytes = key.encode('UTF-16BE')
+            buff = buff + len(key_bytes).to_bytes(4, byteorder='big')
+            buff = buff + key_bytes
 
-                # answer ok
-                response = {'res':'ok'}
-                conn.sendall(marshall(response))
+            value_bytes = value.encode('UTF-16BE')
+            buff = buff + len(value_bytes).to_bytes(4, byteorder='big')
+            buff = buff + value_bytes
+
+        buff = len(buff).to_bytes(4, byteorder='big') + buff
+        self.request.sendall(buff)
+
 
 if __name__ == "__main__":
-    print('Personal license server. Educative purpose only.')
+    print('Personal license server. Educational purpose only.')
     print('WARNING: You MUST apply for an actual license to use the software.')
+    print('WARNING: Do NOT use any software without the required license.')
+    
+    HOST, PORT = "0.0.0.0", 10559
 
-    start()
+    with socketserver.TCPServer((HOST, PORT), RequestHandler) as server:
+        server.serve_forever()
